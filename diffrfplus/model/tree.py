d467cc3e67b68240fc2e43ae490f0286e6df70d7
Davi Yan <davi.yan_ma@yahoo.com>
2025-03-28 21:03:56 -0300

    [feat] add better clustering logic

--- diffrfplus/model/tree.py
+++ diffrfplus/model/tree.py
@@ -1,5 +1,5 @@
 """
-TODO
+DiFF-RF Random Partitioning Tree Class
 """
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
@@ -9,22 +9,23 @@ TODO
 
 import random
 import numpy as np
-
-from .. import utils
-from . import clustering
+from utils import generate_feature_distribution, split_column, cluster_data
 
 class Node:
     """
-    TODO
+    Node in the DiFF-RF decision tree.
+    
+    This class represents either an internal node (with split criteria)
+    or a leaf node (with statistical information about the data it contains).
     """
-    def __init__(self, data, height_limit, feature_distribution=None, sample_size=None, height=0, is_leaf=False):
+    def __init__(self, data, height_limit, feature_distribution=None, sample_size=None, height=0, is_leaf=False, hyperparams={}):
         """
-        TODO
+        Initialize a node in the DiFF-RF tree.
         """
         self.size = len(data)
-        self.algorithm = 'hdbscan'
+
         self.height_limit = height_limit
-        self.height       = height
+        self.height = height
 
         self.feature_distribution = feature_distribution
 
@@ -33,6 +34,8 @@ class Node:
         self.sample_size = sample_size
         self.freq = self.size / sample_size if sample_size else None
 
+        self.hyperparams = {}
+
         if is_leaf:
             self._initialize_leaf(data)
         else:
@@ -40,78 +43,66 @@ class Node:
 
     def _initialize_internal(self, data):
         """
-        TODO
+        Initialize an internal node by finding a split and creating child nodes.
         """
         if len(data) > 32:
-            self.feature_distribution = utils.generate_feature_distribution(data)
+            self.feature_distribution = generate_feature_distribution(data)
 
         cols = np.arange(np.shape(data)[1], dtype='int')
         self.split_feature = random.choices(cols, weights=self.feature_distribution)[0]
-        split_column = data[:, self.split_feature]
+        column = data[:, self.split_feature]
 
-        self.split_value = utils.split(split_column)
+        self.split_value = split_column(column)
 
-        mask = split_column <= self.split_value
+        mask = column <= self.split_value
         data_left = data[mask, :]
 
         next_height = self.height + 1
         limit_reached = (self.height_limit <= self.height or
-                         data_left.shape[0] <= 16 or
+                         data_left.shape[0] <= 5 or
                          np.all(data_left.max(0) == data_left.min(0)))
 
         self.left = Node(data_left if data_left.shape[0] else data,
                          self.height_limit, self.feature_distribution,
                          self.sample_size, next_height,
-                         is_leaf=limit_reached)
+                         is_leaf=limit_reached,
+                         hyperparams=self.hyperparams)
 
         data_right = data[~mask, :]
         limit_reached = (self.height_limit <= self.height or
-                         data_right.shape[0] <= 16 or
+                         data_right.shape[0] <= 5 or
                          np.all(data_right.max(0) == data_right.min(0)))
 
         self.right = Node(data_right if data_right.shape[0] else data,
                          self.height_limit, self.feature_distribution,
                          self.sample_size, next_height,
-                         is_leaf=limit_reached)
+                         is_leaf=limit_reached,
+                         hyperparams=self.hyperparams)
 
         self.n_nodes = 1 + self.left.n_nodes + self.right.n_nodes
 
     def _initialize_leaf(self, data):
         """
-        TODO
+        Initialize a leaf node with statistical information and clustering.
         """
-        self.cluster_model, labels = clustering.build_cluster(data, algorithm=self.algorithm)
-
-        if np.any(labels == -1):
-            labels[labels == -1] = np.max(labels) + 1
-
-        # Averge per cluster.
-        clusters = [np.where(labels == label)[0].tolist() for label in np.unique(labels)]
-
-        self.n_clusters = len(clusters)
-        self.avg = np.zeros((self.n_clusters, np.shape(data)[1]))
-        self.std = np.ones((self.n_clusters, np.shape(data)[1]))
-
-        for label, cluster in enumerate(clusters):
-            self.avg[label] = np.mean(data[cluster, :], axis=0)
-
+        # Basic statistics for all data in the leaf
+        self.avg = np.mean(data, axis=0)
         if len(data) > 10:
-            for label, cluster in enumerate(clusters):
-                self.std[label] = np.std(data[cluster, :], axis=0)
+            self.std = np.std(data, axis=0)
             self.std[self.std == 0] = 1e-2
-
-    def similarity_score(self, instances, alpha):
-        """
-        TODO
-        """
-        d = np.shape(instances)[1]
-        labels = self.cluster_model.predict(instances)
-
-        if len(instances) > 0:
-            avg = self.avg[labels]
-            std = self.std[labels]
-            similarity = (instances - avg) / std
-            similarity = 2**(-alpha * (np.sum((similarity * similarity)/d, axis=1)))
         else:
-            similarity = 0
-        return similarity
+            self.std = np.ones(np.shape(data)[1])
+        
+        min_clustering_points = self.hyperparams.get('min_clustering_points', 10)
+        if len(data) >= min_clustering_points:
+            self.centroids, self.cluster_labels, self.centroid_stds = cluster_data(data, 
+                                                                                   self.hyperparams['model'], 
+                                                                                   self.hyperparams)
+            
+            if len(self.centroids) <= 1:
+                self.centroids = np.array([self.avg])
+                self.centroid_stds = np.array([self.std])
+        else:
+            # Not enough data for meaningful clustering
+            self.centroids = np.array([self.avg])
+            self.centroid_stds = np.array([self.std])
